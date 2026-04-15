674a699ee9a5bf1e5f8142f297319992c31599bf
Sujong121 <cssj2000@naver.com>
2026-04-15 21:30:29 +0900

    fix: API URL 환경변수로 변경

--- src/utils/saveLogToDB.js
+++ src/utils/saveLogToDB.js
@@ -37,7 +37,7 @@ export const saveLogToDB = async (logData) => {
   };
 
   try {
-    await axios.post('invigorating-analysis-production-6785.up.railway.app/api/ux-logs/session', {
+    await axios.post('{import.meta.env.VITE_API_URL}/api/ux-logs/session', {
       session_id: logData.session_id,
       log_json: JSON.stringify(orderedLog)
     });
